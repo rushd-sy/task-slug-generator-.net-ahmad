dce3e40c7c66556b870f980112dcfd6f902cca24
AhmadDabbagh <ahmadbesherdabbagh@gmail.com>
2026-03-19 17:01:08 +0300

    update: Generate method

--- SlugGenerator/SlugGenerator/Class1.cs
+++ SlugGenerator/SlugGenerator/Class1.cs
@@ -1,4 +1,6 @@
-﻿namespace SlugGenerator
+﻿using System.Text.RegularExpressions;
+
+namespace SlugGenerator
 {
     public class Class1
     {
@@ -6,7 +8,20 @@
     }
     public static class Genrate
     {
-        public static string Generate(string text) => text;
-        
+        public static string Generate(string text)
+        {
+
+            text = text.ToLower();
+            text = text.Trim();
+
+
+            text = Regex.Replace(text, @"[^\p{L}\d\s-_]", "");
+            text = Regex.Replace(text, @"\s+", "-");
+            text = Regex.Replace(text, @"_+", "-");
+            text = Regex.Replace(text, @"-+", "-");
+
+            return text;
+        }
+
     }
 }
