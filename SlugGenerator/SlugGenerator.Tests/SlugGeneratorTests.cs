c673bae02ab23f6aaffd685286b614daa4967c8f
AhmadDabbagh <ahmadbesherdabbagh@gmail.com>
2026-03-24 22:29:52 +0300

    feat: add x uint test project (SlugGenerator.Tests)

---
+++ SlugGenerator/SlugGenerator.Tests/SlugGeneratorTests.cs
@@ -0,0 +1,68 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SlugGenerator.Tests
+{
+    public class SlugGeneratorTests
+    {
+        [Fact]
+        public void Generate_TextIsNull_ThrowArgumentNullException()
+        {
+            string text = null;
+
+            Func<string, string> func = (e) => SlugGenerator.Generate(text);
+
+            Assert.Throws<ArgumentNullException>(() => func(text));
+
+        }
+
+        [Fact]
+        public void Generate_TextIsEmpty_ThrowArgumentException()
+        {
+            string text = "";
+
+            Func<string, string> func = (e) => SlugGenerator.Generate(text);
+
+            Assert.Throws<ArgumentException>(() => func(text));
+
+        }
+
+        [Fact]
+        public void Generate_ForTextStandardinputs_ReturnSlugText()
+        {
+            string text = "A__B C";
+
+            var actual = SlugGenerator.Generate(text);
+
+            var expected = "a-b-c";
+
+            Assert.Matches(expected, actual);
+
+
+        }
+
+        [Fact]
+        public void Generate_ForTextsContainOnlySymbols_ThrowArgumentException()
+        {
+            string text = "@#$$";
+
+            Func<string, string> func = (e) => SlugGenerator.Generate(text);
+
+            Assert.Throws<ArgumentException>(() => func(text));
+        }
+
+        [Fact]
+        public void Generate_ForTextsArabicInputs_()
+        {
+            string text = "شقة للبيع";
+
+            var actual = SlugGenerator.Generate(text);
+
+            var expected = "شقة-للبيع";
+
+
+            Assert.Matches(expected, actual);
+
+
+        }
+    }
+}
---
+++ SlugGenerator/SlugGenerator/SlugGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SlugGenerator
+{
+    public static class SlugGenerator
+    {
+        public static string Generate(string text)
+        {
+            if(text is null)
+              throw new ArgumentNullException("Text");
+
+            if(text.Length == 0)
+                throw new ArgumentException("text is empty");
+
+            Regex regex = new Regex("[\u0600-\u06ff]");
+           
+
+            text = text.ToLower().Trim();
+            
+            text = Regex.Replace(text, @"[^\p{L}\d\s-_]", "");
+
+            if (text.Length == 0)
+                throw new ArgumentException("text contain only symbols");
+
+            text = Regex.Replace(text, @"[\s_-]+", "-");
+
+
+            return text;
+        }
+      
+    }
+}
